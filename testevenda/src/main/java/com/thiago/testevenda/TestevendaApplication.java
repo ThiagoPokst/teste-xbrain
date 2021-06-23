6d29eb63ad50c2adc46d31ef782b4e7b9bbd64c2
ThiagoPokst <31635628+ThiagoPokst@users.noreply.github.com>
2021-06-23 11:54:29 -0300

    Add files via upload

---
+++ testevenda/src/main/java/com/thiago/testevenda/TestevendaApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.testevenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestevendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestevendaApplication.class, args);
+	}
+
+}
---
+++ testevenda/src/test/java/com/thiago/testevenda/TestevendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.testevenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestevendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
